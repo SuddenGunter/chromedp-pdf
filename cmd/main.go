98496458dcb7aa688ee0ba074ce0ef5340ca60fe
Artem Kolomytsev <kolomytsev1996@gmail.com>
2019-06-26 00:49:33 +0300

    added modules, added simple main just for test, added Dockerfile

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"github.com/chromedp/chromedp"
+	"io/ioutil"
+	"log"
+)
 
-func main()  {
-	fmt.Print("hello")
+
+func main() {
+	// create context
+	ctx, cancel := chromedp.NewContext(context.Background())
+	defer cancel()
+
+	// run task list
+	var buf []byte
+	err := chromedp.Run(ctx, screenshot(`https://www.google.com/`, `#main`, &buf))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// save the screenshot to disk
+	if err = ioutil.WriteFile("screenshot.png", buf, 0644); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func screenshot(urlstr, sel string, res *[]byte) chromedp.Tasks {
+	return chromedp.Tasks{
+		chromedp.Navigate(urlstr),
+		chromedp.WaitVisible(sel, chromedp.ByID),
+		chromedp.Screenshot(sel, res, chromedp.NodeVisible, chromedp.ByID),
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
-module h2p
+module github.com/SuddenGunter/pandaren
 
 go 1.12
+
+require github.com/chromedp/chromedp v0.3.0
